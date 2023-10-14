300e64e8f4c742f8a290467a4b7d38f638bfd7f8
Anthony <anthonynwobodo7@gmail.com>
2023-10-14 08:46:27 +0100

    :sparkles: (Amazon Linux Script)

---
+++ amazon-linux-user-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+
+echo "<h1>Hello World From Anthony's Amazon Linux @ $(hostname -f)</h1>" > /var/www/html/index.html
